5117d6175b193ba801e815e1a1f55363a800db5b
Ahashm <ahmat18@student.sdu.dk>
2022-12-02 15:39:50 +0100

    NLU changes

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var router = express.Router();
+var dialogflowfulfillment = require("dialogflow-fulfillment");
 
 /* GET users listing. */
-router.post("/", function (req, res, next) {
-  console.log(req.body);
+router.post("/", express.json(), function (req, res) {
+  const agent = new dialogflowfulfillment.WebhookClient({
+    request: req,
+    response: res,
+  });
+
+  function test(agent) {
+    agent.add("Stormzy fra Webhook server");
+  }
+
+  var intentMap = new Map();
+
+  intentMap.set("TestDemo", test);
+  agent.handleRequest(intentMap);
+});
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
