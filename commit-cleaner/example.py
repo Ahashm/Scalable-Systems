2538a0305e58b72ef92d86178a06c2d41d64357b
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-12-06 11:50:05 +0100

    Update example.py

--- commit-cleaner/example.py
+++ commit-cleaner/example.py
@@ -23,7 +23,6 @@ def createFileStructureSchema():
         .add("changes", IntegerType())\
         .add("status", StringType())
 
-# Create SparkSession and configure it
 spark = SparkSession.builder.appName('streamTest') \
     .config('spark.master','spark://spark-master:7077') \
     .config('spark.executor.cores', 1) \
@@ -32,7 +31,6 @@ spark = SparkSession.builder.appName('streamTest') \
     .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
     .getOrCreate()
 
-# Create a read stream from Kafka and a topic
 df = spark \
     .readStream \
     .format("kafka") \
@@ -42,7 +40,7 @@ df = spark \
     .load()
 
 structureSchema = StructType()\
-    .add("commit", StructType().add("author", StructType().add("name", StringType()).add("email", StringType()).add("date", DateType())))\
+    .add("commit", StructType().add("author", StructType().add("name", StringType()).add("email", StringType()).add("date", TimestampType())))\
     .add("url", StringType())\
     .add("sha", StringType())\
     .add("stats", StructType().add("additions", IntegerType()).add("deletions", IntegerType()).add("total", IntegerType()))\
@@ -55,16 +53,9 @@ data_as_json = data_as_json.withColumn("repo", getRepoName(data_as_json.url))
 data_as_json = data_as_json.select(col("commit.author").alias("commit_author"), col("stats"), col("files"), col("repo"), col("sha").alias("id"))
 
 data_as_json.select(to_json(struct([data_as_json[x] for x in data_as_json.columns])).alias("value")).select("value")\
-        .writeStream\
-        .format('kafka')\
-        .option("kafka.bootstrap.servers", "kafka:9092") \
-        .option("topic", "sentiment-scores") \
-        .outputMode("append") \
-        .start().awaitTermination(100).stop()
-# Cast to string
-#[data_as_json[0] for x in data_as_json.columns]
-
-# Create a Kafka write stream, with the output mode "complete"
-#json_commit = commit.select(from_json(col("value.from_json(value)"), structureSchema)).alias("commit")#.select("commit")
-
-#print(json_commit)
+    .writeStream\
+    .format('kafka')\
+    .option("kafka.bootstrap.servers", "kafka:9092") \
+    .option("topic", "clean-commits") \
+    .outputMode("append") \
+    .start().awaitTermination(100).stop()
