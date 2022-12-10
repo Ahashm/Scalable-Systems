969e46467adf5e97497f1aa7fc8ff6e605e81a3f
Frederikh1 <42929762+Frederikh1@users.noreply.github.com>
2022-12-10 18:04:24 +0100

    working overview create

--- history_processing/example.py
+++ history_processing/example.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import from_json, explode, split, to_json, array, col, struct, udf, lit
+from pyspark.sql.functions import sum, explode, to_json, array, col, struct, udf, lit, collect_list
 import locale
 locale.getdefaultlocale()
 locale.getpreferredencoding()
@@ -37,27 +37,13 @@ files.printSchema()
 files.show()
 
 per_file = files.withColumn("file", explode(files.files))
-per_file = per_file.withColumn("filename", per_file.file.filename)
-per_file = per_file.select(col("repo"), col("commit_author"), col("file"), col("filename"))
+per_file = per_file.select(col("repo"), col("commit_author"), col("file"))
 per_file = per_file.withColumn("added_lines", totalAdditions(per_file.file))
-per_file = per_file.withColumn("file_type", getFileType(per_file.fileName))
+per_file = per_file.withColumn("filetype", getFileType(per_file.file.fileName))
+per_file = per_file.groupBy("repo", "filetype").agg(sum("added_lines").alias("added_lines"))
+per_file = per_file.groupBy("repo").agg(sum("added_lines").alias("total_lines"),collect_list(struct("filetype", "added_lines")).alias("filetypes"))
 
-per_file.printSchema()
-per_file.show()
+per_file.write.format("mongodb").mode("overwrite").option("database",
+"spotify").option("collection", "overview").save()
 
-total_collection = per_file.reduce
-# Read in all files in the directory
-#txtFiles = sc.wholeTextFiles(files, 20)
-# Take the content of the files and split them
-#all_word = txtFiles.flatMap(lambda s: s[1].split())
-# Change from list of words to list of (word, 1)
-#word_map = all_word.map(lambda s: (s, 1))
-# Merge values with equal keys
-#word_reduce = word_map.reduceByKey(lambda s, t: s+t)
-# Filter using the defined lambda and sort by value
-#top_words = word_reduce.filter(select_words).sortBy(lambda s: s[1])
-# Save as text file
-#top_words.saveAsTextFile('hdfs://namenode:9000/txt-out')
-# Collect to a Python list and print
-#print(top_words.collect())
 
